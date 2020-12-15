632d9727c3c4c3008e4994f9d811c341650bd9ca
Bastien <blaucournet@myges.fr>
2020-12-15 19:03:07 +0100

    encryption success 100%

--- main.c
+++ main.c
@@ -12,114 +12,119 @@ int main(int argc, char *argv[])
     char letter_1[8] = {0};
     char letter_2[8] = {0};
     char letter_final[8];
-    int final_letter;
-    int i = 7;
+    char final_letter;
+    int i;
     int lettre;
     int check_word = 0;
+    int choice = 0;
+
+    if(choice == 0){
     
-    printf("choose the file to decrypt : ");
+        printf("choose the file to decrypt : ");
 
-    fgets(file_crypt, 255, stdin);
+        fgets(file_crypt, 255, stdin);
 
-        if(file_crypt[strlen(file_crypt) - 1] == '\n')
-            file_crypt[strlen(file_crypt) - 1] = '\0';
+            if(file_crypt[strlen(file_crypt) - 1] == '\n')
+                file_crypt[strlen(file_crypt) - 1] = '\0';
 
     
-    for(i = 0; i < strlen(file_crypt) - 1; i++){
+        for(i = 0; i < strlen(file_crypt) - 1; i++){
 
-        file_decrypt[i] = file_crypt[i];
-    }
+            file_decrypt[i] = file_crypt[i];
+        }
 
-    FILE *fic = fopen(file_crypt, "rb");
+        FILE *fic = fopen(file_crypt, "rb");
 
-    FILE *fic2 = fopen(file_decrypt, "wb");
+        FILE *fic2 = fopen(file_decrypt, "wb");
 
-    i = 7;
+        i = 7;
 
-    while((lettre = fgetc(fic)) != EOF){
+        while((lettre = fgetc(fic)) != EOF){
 
-        if(check_word == 0){
+            if(check_word == 0){
 
-            while(lettre != 0){
+                while(lettre != 0){
 
-                letter_1[i] = lettre % 2;
+                    letter_1[i] = lettre % 2;
+
+                    lettre = lettre / 2;
+
+                    i--;
+
+                }
+    
+                check_word = 1;
+
+                i = 7;
+
+            }else{
+  
+                while(lettre != 0){
+
+                letter_2[i] = lettre % 2;
 
                 lettre = lettre / 2;
 
                 i--;
 
-            }
-    
-        check_word = 1;
-
-        i = 7;
-
-        }else{
-  
-            while(lettre != 0){
-
-            letter_2[i] = lettre % 2;
-
-            lettre = lettre / 2;
-
-            i--;
-
             }   
         
-            check_word = 0;
+                check_word = 0;
 
-            letter_final[0] = letter_1[4];
-            letter_final[1] = letter_1[1];
-            letter_final[2] = letter_1[2];
-            letter_final[3] = letter_1[3];
+                letter_final[0] = letter_1[4];
+                letter_final[1] = letter_1[1];
+                letter_final[2] = letter_1[2];
+                letter_final[3] = letter_1[3];
             
-            letter_final[4] = letter_2[4];
-            letter_final[5] = letter_2[1];
-            letter_final[6] = letter_2[2];
-            letter_final[7] = letter_2[3];
+                letter_final[4] = letter_2[4];
+                letter_final[5] = letter_2[1];
+                letter_final[6] = letter_2[2];
+                letter_final[7] = letter_2[3];
 
-            if(letter_final[0] == 1)
-                final_letter = final_letter + 128;
+                if(letter_final[0] == 1)
+                    final_letter = final_letter + 128;
             
-            if(letter_final[1] == 1)
-                final_letter = final_letter + 64;
+                if(letter_final[1] == 1)
+                    final_letter = final_letter + 64;
             
-            if(letter_final[2] == 1)
-                final_letter = final_letter + 32;
+                if(letter_final[2] == 1)
+                    final_letter = final_letter + 32;
                 
-            if(letter_final[3] == 1)
-                final_letter = final_letter + 16;
+                if(letter_final[3] == 1)
+                    final_letter = final_letter + 16;
                 
-            if(letter_final[4] == 1)
-                final_letter = final_letter + 8;
+                if(letter_final[4] == 1)
+                    final_letter = final_letter + 8;
 
-            if(letter_final[5] == 1)
-                final_letter = final_letter + 4;
+                if(letter_final[5] == 1)
+                    final_letter = final_letter + 4;
 
-            if(letter_final[6] == 1)
-                final_letter = final_letter + 2;
+                if(letter_final[6] == 1)
+                    final_letter = final_letter + 2;
 
-            if(letter_final[7] == 1)
-                final_letter = final_letter + 1;
+                if(letter_final[7] == 1)
+                    final_letter = final_letter + 1;
 
-            for(i = 0; i < 8; i++){
+                for(i = 0; i < 8; i++){
     
-                letter_1[i] = 0;
+                    letter_1[i] = 0;
 
-                letter_2[i] = 0;
+                    letter_2[i] = 0;
 
-            }
+                }
 
-            fputc(final_letter, fic2);
+                fputc(final_letter, fic2);
 
-            final_letter = 0;
+                final_letter = 0;
 
-            i = 7;
+                i = 7;
 
-        }   
+            }   
+        }
+
+        fclose(fic);
+        fclose(fic2);
+
     }
-
-    fclose(fic);
-    fclose(fic2);
 
 }

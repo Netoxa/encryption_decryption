a5d1a83924ef6df3ed05e93344aaddadc88d3792
Bastien <blaucournet@myges.fr>
2020-12-16 14:35:26 +0100

    add function Binary_to_Decimal

--- main.c
+++ main.c
@@ -5,6 +5,26 @@
 #include <string.h>
 #include <time.h>
 
+
+
+void Binary_to_Decimal(char *array, char final_letter, FILE *fic2){
+
+    int i;
+    int number = 128;
+
+    for(i = 0; i < 8; i++){
+
+        if(array[i] == 1)
+            final_letter = final_letter + number;
+
+        number = number / 2;
+
+    }
+
+    fputc(final_letter, fic2);
+
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -88,29 +108,7 @@ int main(int argc, char *argv[])
                 letter_final[6] = letter_2[2];
                 letter_final[7] = letter_2[3];
 
-                if(letter_final[0] == 1)
-                    final_letter = final_letter + 128;
-            
-                if(letter_final[1] == 1)
-                    final_letter = final_letter + 64;
-            
-                if(letter_final[2] == 1)
-                    final_letter = final_letter + 32;
-                
-                if(letter_final[3] == 1)
-                    final_letter = final_letter + 16;
-                
-                if(letter_final[4] == 1)
-                    final_letter = final_letter + 8;
-
-                if(letter_final[5] == 1)
-                    final_letter = final_letter + 4;
-
-                if(letter_final[6] == 1)
-                    final_letter = final_letter + 2;
-
-                if(letter_final[7] == 1)
-                    final_letter = final_letter + 1;
+                Binary_to_Decimal(letter_final, final_letter, fic2);
 
                 for(i = 0; i < 8; i++){
     
@@ -119,8 +117,6 @@ int main(int argc, char *argv[])
                     letter_2[i] = 0;
 
                 }
-
-                fputc(final_letter, fic2);
 
                 final_letter = 0;
 
@@ -180,68 +176,19 @@ int main(int argc, char *argv[])
             letter_2[6] = rand() % 2;
             letter_2[7] = rand() % 2;
 
+            Binary_to_Decimal(letter_1, final_letter, fic2);
 
-                if(letter_1[0] == 1)
-                    final_letter = final_letter + 128;
-            
-                if(letter_1[1] == 1)
-                    final_letter = final_letter + 64;
-            
-                if(letter_1[2] == 1)
-                    final_letter = final_letter + 32;
-                
-                if(letter_1[3] == 1)
-                    final_letter = final_letter + 16;
-                
-                if(letter_1[4] == 1)
-                    final_letter = final_letter + 8;
+            final_letter = 0;
 
-                if(letter_1[5] == 1)
-                    final_letter = final_letter + 4;
+            Binary_to_Decimal(letter_2, final_letter, fic2);
 
-                if(letter_1[6] == 1)
-                    final_letter = final_letter + 2;
+            final_letter = 0;
 
-                if(letter_1[7] == 1)
-                    final_letter = final_letter + 1;
-
-                fputc(final_letter, fic2);
-
-                final_letter = 0;
-
-                if(letter_2[0] == 1)
-                    final_letter = final_letter + 128;
-            
-                if(letter_2[1] == 1)
-                    final_letter = final_letter + 64;
-            
-                if(letter_2[2] == 1)
-                    final_letter = final_letter + 32;
-                
-                if(letter_2[3] == 1)
-                    final_letter = final_letter + 16;
-                
-                if(letter_2[4] == 1)
-                    final_letter = final_letter + 8;
-
-                if(letter_2[5] == 1)
-                    final_letter = final_letter + 4;
-
-                if(letter_2[6] == 1)
-                    final_letter = final_letter + 2;
-
-                if(letter_2[7] == 1)
-                    final_letter = final_letter + 1;
-
-                fputc(final_letter, fic2);
-
-                final_letter = 0;
-
-                for(i = 0; i < 8; i++){
+            for(i = 0; i < 8; i++){
     
-                    letter_final[i] = 0;
-
-                }
+                letter_final[i] = 0;
+                
+            }
                 
             i = 7;
             

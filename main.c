4ee75b10e284d0b91f8e48e5eba27414d76d6cbe
Netoxa <bastienlauc@gmail.com>
2021-01-12 01:33:37 +0100

    matrix dynamique

--- main.c
+++ main.c
@@ -72,21 +72,30 @@ int Check_bits_Matrix(char *file, FILE *fic, int i, int j, int position){
 
 }
 
-
-
 int main(int argc, char *argv[])
 {
 
     char file[256];
-    int letter_1[8];
-    int letter_2[8];
-    int letter_final[8];
+    int letter_1[8] = {0};
+    int letter_2[8] = {0};
+    int letter_final[8] = {0};
+    char matrix[4][255];
+    char matrix_2[8][9];
+    char matrix_search[4][255] =  {
+                                    {"1000"},
+                                    {"0100"},
+                                    {"0010"},
+                                    {"0001"}
+                                };
+    int matrix_result[4] = {0};
     char final_letter = 0;
     int i;
     int j = 0;
     int letter;
     int check_word = 0;
     int choice;
+    FILE *fic;
+    FILE *fic2;
 
     while(j != 38){
 
@@ -98,7 +107,7 @@ int main(int argc, char *argv[])
 
             file[strlen(file) - 1] = '\0';
 
-        FILE *fic = fopen(file, "rb");
+        fic = fopen(file, "rb");
 
         fgets(file, 255, fic);
 
@@ -134,7 +143,49 @@ int main(int argc, char *argv[])
 
     }
 
-    printf("\n The matrix is correct \n\n What do you want to do ? :\n 0 : Decrypt a file\n Any other number : Crypt a file\n Your answer : ");
+    printf("\n The matrix is correct");
+
+    rewind(fic);
+
+    fscanf(fic, "%s %s %s %s", matrix[0], matrix[1], matrix[2], matrix[3]);
+
+    strncpy(matrix[0], matrix[0] + 5, 255);
+
+    matrix[3][strlen(matrix[3]) - 1] = '\0';
+
+    for(i = 0; i < 4; i++){
+
+        matrix_2[0][i] = matrix[i][0];
+        matrix_2[1][i] = matrix[i][1];
+        matrix_2[2][i] = matrix[i][2];
+        matrix_2[3][i] = matrix[i][3];
+        matrix_2[4][i] = matrix[i][4];
+        matrix_2[5][i] = matrix[i][5];
+        matrix_2[6][i] = matrix[i][6];
+        matrix_2[7][i] = matrix[i][7];
+        
+    }
+
+    for(i = 0; i < 8; i++){
+
+        for(j = 0; j < 4; j++){
+
+            if(strcmp(matrix_2[i], matrix_search[j]) == 0)
+
+                matrix_result[j] = i;
+                
+        }
+    }
+
+    fclose(fic);
+
+    printf("\n The key is ");
+
+    for(i = 0; i < 4; i++)
+
+        printf("%d", matrix_result[i]);
+
+    printf("\n\n What do you want to do ? :\n 0 : Decrypt a file\n Any other number : Crypt a file\n Your answer : ");
 
     scanf("%d", &choice);
 
@@ -150,11 +201,11 @@ int main(int argc, char *argv[])
 
             file[strlen(file) - 1] = '\0';
 
-        FILE *fic = fopen(file, "rb");
+        fic = fopen(file, "rb");
 
         file[strlen(file) - 1] = '\0';
 
-        FILE *fic2 = fopen(file, "wb");
+        fic2 = fopen(file, "wb");
 
         while((letter = fgetc(fic)) != EOF){
 
@@ -188,21 +239,21 @@ int main(int argc, char *argv[])
 
                 check_word = 0;
 
-                letter_final[0] = letter_1[4];
-                letter_final[1] = letter_1[1];
-                letter_final[2] = letter_1[2];
-                letter_final[3] = letter_1[3];
+                letter_final[0] = letter_1[matrix_result[0]];
+                letter_final[1] = letter_1[matrix_result[1]];
+                letter_final[2] = letter_1[matrix_result[2]];
+                letter_final[3] = letter_1[matrix_result[3]];
 
-                letter_final[4] = letter_2[4];
-                letter_final[5] = letter_2[1];
-                letter_final[6] = letter_2[2];
-                letter_final[7] = letter_2[3];
+                letter_final[4] = letter_2[matrix_result[0]];
+                letter_final[5] = letter_2[matrix_result[1]];
+                letter_final[6] = letter_2[matrix_result[2]];
+                letter_final[7] = letter_2[matrix_result[3]];
 
                 Binary_to_Decimal(letter_final, final_letter, fic2);
 
                 for(i = 0; i < 8; i++){
 
-                    letter_1[i] = 0;
+                    letter_1[i] = 0;    
 
                     letter_2[i] = 0;
 
@@ -221,11 +272,11 @@ int main(int argc, char *argv[])
         if(file[strlen(file) - 1] == '\n')
             file[strlen(file) - 1] = '\0';
 
-        FILE *fic = fopen(file, "rb");
+        fic = fopen(file, "rb");
 
         strcat(file, "c");
 
-        FILE *fic2 = fopen(file, "wb");
+        fic2 = fopen(file, "wb");
 
         while((letter = fgetc(fic)) != EOF){
 
@@ -242,22 +293,24 @@ int main(int argc, char *argv[])
             }
 
             letter_1[0] = rand() % 2;
-            letter_1[1] = letter_final[1];
-            letter_1[2] = letter_final[2];
-            letter_1[3] = letter_final[3];
-            letter_1[4] = letter_final[0];
             letter_1[5] = rand() % 2;
             letter_1[6] = rand() % 2;
             letter_1[7] = rand() % 2;
 
+            letter_1[matrix_result[0]] = letter_final[0];
+            letter_1[matrix_result[1]] = letter_final[1];
+            letter_1[matrix_result[2]] = letter_final[2];
+            letter_1[matrix_result[3]] = letter_final[3];
+
             letter_2[0] = rand() % 2;
-            letter_2[1] = letter_final[5];
-            letter_2[2] = letter_final[6];
-            letter_2[3] = letter_final[7];
-            letter_2[4] = letter_final[4];
             letter_2[5] = rand() % 2;
             letter_2[6] = rand() % 2;
             letter_2[7] = rand() % 2;
+
+            letter_2[matrix_result[0]] = letter_final[4];
+            letter_2[matrix_result[1]] = letter_final[5];
+            letter_2[matrix_result[2]] = letter_final[6];
+            letter_2[matrix_result[3]] = letter_final[7];
 
             Binary_to_Decimal(letter_1, final_letter, fic2);
 
